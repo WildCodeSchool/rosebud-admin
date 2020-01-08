83908887af805af4a9e8e6549094eae693f71e2e
François Boisselier <francois.boisselier@gmail.com>
2020-01-08 10:31:47 +0100

    list of questionnaires

--- rosebud-admin/src/App.js
+++ rosebud-admin/src/App.js
@@ -1,19 +1,18 @@
-import React, {useState} from 'react';
-import { Admin, Resource } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
-import UserList from './UserList'
-import authProvider from './authProvider';
-import axios from 'axios'
+import React, { useState } from "react";
+import { Admin, Resource } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+//import UserList from './UserList'
+import authProvider from "./authProvider";
+import Questionnaires from "./Questionnaires";
 
-console.log(authProvider)
-const App = () => (
-  <Admin dataProvider={jsonServerProvider('http://localhost:3001')} authProvider={authProvider} >
-      <Resource name="users" list={UserList} />
-  </Admin>
-);
-const [questionList, setquestionList] = useState()
+function App() {
 
-axios.get ('api/v1/questionnaires')
-  .then(res => )
+  <Admin
+    dataProvider={jsonServerProvider("/api/v1/questionnaires")}
+    authProvider={authProvider}
+  >
+    <Resource name="questionnaires" list={Questionnaires} />
+  </Admin>;
+}
 
 export default App;
