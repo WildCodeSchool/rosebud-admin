26126369580019d12aff15448b45e72987211b25
FPaurisse <paurissefrederic@gmail.com>
2020-01-30 15:38:41 +0100

    delete baseURL img

--- src/components/Participant/ParticipantShow.js
+++ src/components/Participant/ParticipantShow.js
@@ -53,7 +53,7 @@ const ParticipantShow = props => {
                 </ReferenceField>
                 <FormDataConsumer>
                     {({ record }) =>
-                    <img src={baseURL ? baseURL + record.image_url : record.image_url } alt="Answer" width="200px" />
+                    <img src={record.image_url.includes(baseURL) ? record.image_url : baseURL + record.image_url} alt="Answer" width="200px" />
                     }
                 </FormDataConsumer>
                 <TextField source="comment" label="Commentaire" />
