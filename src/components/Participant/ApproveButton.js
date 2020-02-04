c787a1b19201f032af28694789e104e9536c9c4f
FPaurisse <paurissefrederic@gmail.com>
2020-02-04 13:57:37 +0100

    send mail if approved

--- src/components/Participant/ApproveButton.js
+++ src/components/Participant/ApproveButton.js
@@ -1,16 +1,20 @@
 import React, {useState} from 'react';
 import { useMutation, Button } from 'react-admin';
 import DoneIcon from '@material-ui/icons/Done';
-import {Redirect} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 const ApproveButton = ({ participantId, redirect }) => {
     const [approve, { loading }] = useMutation({
         type: 'update',
-        resource: 'participants',
+        resource: 'participants', 
         payload: { id: participantId, data: { isApproved: true } }
     });
+    
     const [redirectAction, setRedirectAction] = useState(false);
     
+    const sendEmail = axios.post(`/api/back/v1/participants/${participantId}/approved`);
+
     return (
         <>
             <Button
@@ -18,6 +22,7 @@ const ApproveButton = ({ participantId, redirect }) => {
                 onClick={() => {
                     approve();
                     setRedirectAction(true);
+                    sendEmail();
                 }}
                 disabled={loading}
                 type="submit"
